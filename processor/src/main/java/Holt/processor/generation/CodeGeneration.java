af34aeeb1c0a04eb32f9cd6b3398c039af5fe4d5
Tobias Karlsson <tobiakar@student.chalmers.se>
2022-02-21 11:29:48 +0100

    Generate interface from annotation

--- processor/src/main/java/Holt/processor/generation/CodeGeneration.java
+++ processor/src/main/java/Holt/processor/generation/CodeGeneration.java
@@ -2,6 +2,7 @@ package Holt.processor.generation;
 
 import com.squareup.javapoet.*;
 
+import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import java.io.IOException;
 import java.util.Map;
@@ -83,56 +84,97 @@ public class CodeGeneration {
         return JavaFile.builder("Holt.codeGeneration.custom", classGen).build();
     }
 
+    public static JavaFile genereateMethodFromAnnotation(Element element, Class input, Class output, String methodName) {
+        // TODO: What about multiple inputs?
+        // Maybe have input be a List<Class> inputs, alternatively with names as well
+        MethodSpec method = MethodSpec.methodBuilder(methodName)
+                .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+                .returns(output)
+                .addParameter(input, "input")
+                .build();
+
+        TypeSpec interfaceGen = TypeSpec.interfaceBuilder(element.getSimpleName().toString())
+                .addModifiers(Modifier.PUBLIC)
+                .addMethod(method)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder(PACKAGE_NAME, interfaceGen)
+                .build();
+
+        print(javaFile);
+
+        return javaFile;
+    }
+
     public static JavaFile generateCustomProcess(String name, String interfaceName) {
         JavaFile javaFile = generateProcessImpl(name, interfaceName);
-        saveAndPrint(javaFile);
+        print(javaFile);
         return javaFile;
     }
 
     public static JavaFile generateExternalEntity(String name, String interfaceName) {
         JavaFile javaFile = generateProcessExt(name, interfaceName);
-        saveAndPrint(javaFile);
+        print(javaFile);
         return javaFile;
     }
 
     public static JavaFile generateReasonProcess(String name, String interfaceName) {
         JavaFile javaFile = generateProcessExt(name, interfaceName);
-        saveAndPrint(javaFile);
+        print(javaFile);
         return javaFile;
     }
 
     public static JavaFile generateRequestProcess(String name, String interfaceName) {
         JavaFile javaFile = generateProcessExt(name, interfaceName);
-        saveAndPrint(javaFile);
+        print(javaFile);
         return javaFile;
     }
 
     public static JavaFile generateLimitProcess(String name, String interfaceName) {
         JavaFile javaFile = generateProcessExt(name, interfaceName);
-        saveAndPrint(javaFile);
+        print(javaFile);
         return javaFile;
     }
 
     public static JavaFile generateLogProcess(String name, String interfaceName) {
         JavaFile javaFile = generateProcessExt(name, interfaceName);
-        saveAndPrint(javaFile);
+        print(javaFile);
         return javaFile;
     }
 
     public static JavaFile generateLogDBProcess(String name, String interfaceName) {
         JavaFile javaFile = generateProcessExt(name, interfaceName);
-        saveAndPrint(javaFile);
+        print(javaFile);
         return javaFile;
     }
 
 
-    static void saveAndPrint(JavaFile javaFile) {
+    static void print(JavaFile javaFile) {
         try {
             // print
             javaFile.writeTo(System.out);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    static void print(TypeSpec typeSpec) {
+        try {
+            // print
+            System.out.println(typeSpec.toString());
+            //javaFile.writeTo(System.out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void print(AnnotationSpec spec) {
+        try {
+            // print
+            System.out.println(spec.toString());
+            //javaFile.writeTo(System.out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
