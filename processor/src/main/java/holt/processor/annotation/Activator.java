0965a7d38faca9ad769c65509594b37392a1ad24
Tobias Karlsson <tobiakar@student.chalmers.se>
2022-02-23 14:43:21 +0100

    Add @Target to Query and Activator

--- processor/src/main/java/holt/processor/annotation/Activator.java
+++ processor/src/main/java/holt/processor/annotation/Activator.java
@@ -1,5 +1,9 @@
 package holt.processor.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
 public @interface Activator {
     String methodName();
     Class<?> outputType();
--- processor/src/main/java/holt/processor/annotation/Query.java
+++ processor/src/main/java/holt/processor/annotation/Query.java
@@ -1,5 +1,9 @@
 package holt.processor.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
 public @interface Query {
     Class value();
 }
