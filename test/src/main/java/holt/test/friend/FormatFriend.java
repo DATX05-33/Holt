274181ea2b2123f88da12b559f3cd4c1ad7d7e3b
Tobias Karlsson <tobiakar@student.chalmers.se>
2022-03-02 14:37:03 +0100

    Delete old examples

--- test/src/main/java/holt/test/friend/FormatFriend.java
+++ test/src/main/java/holt/test/friend/FormatFriend.java
@@ -2,8 +2,6 @@ package holt.test.friend;
 
 import holt.processor.annotation.Activator;
 import holt.processor.annotation.PADFD;
-import holt.processor.generation.interfaces.IFormatFriend;
-import holt.processor.generation.interfaces.IFriendsDBToFormatFriendQuery;
 
 @Activator(
         methodName = "formatFriend",
@@ -12,25 +10,4 @@ import holt.processor.generation.interfaces.IFriendsDBToFormatFriendQuery;
 @PADFD(name = "friend", file = "friend-padfd.csv")
 public class FormatFriend {
 
-    // first
-    // generate IFormatFriend and IFriendDBToFormatFriendQuery
-    //      IFormatFriend har formatFriend som är givet av Activator
-    //          och FriendDBQueryToFormatFriendQuery som är genererad pga pilen från FriendDB
-    /*public FriendDBQueryToFormatFriendQuery getFriendDBQuery(FriendID id){
-        return db -> {
-            return db.get(id);
-        };
-    }
-
-    // then
-    @Query(FriendRaw.class)
-    public FriendDBQueryToFormatFriendQuery getFriendDBQuery(FriendID id){
-        return db -> {
-            return db.get(id);
-        };
-    }
-
-    public Friend formatFriend(FriendID id, FriendRaw raw){
-        return new Friend(raw.toString());
-    }*/
 }
--- test/src/main/java/holt/test/friend/FriendsDB.java
+++ test/src/main/java/holt/test/friend/FriendsDB.java
@@ -4,9 +4,4 @@ import holt.processor.annotation.DBActivator;
 
 @DBActivator
 public class FriendsDB {
-
-    public FriendRaw getById(FriendID id) {
-        return new FriendRaw(id, "Smurf", "Smurfsson");
-    }
-
 }
